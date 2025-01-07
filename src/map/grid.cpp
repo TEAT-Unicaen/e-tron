12dc75a53a8dda47fe15e7634b4966c6a0e9d3c7
Hysteryx Théo.Sicot <theo.sicot.dev@gmail.com>
2025-01-07 18:45:31 +0100

    Syntax fix

--- src/map/grid.cpp
+++ src/map/grid.cpp
@@ -2,6 +2,35 @@
 
 Grid::Grid(int line, int column) : line(line), column(column) {
 
-	this.grid = new Cell[line][column];
+	if (line <= 0 || column <= 0) {
+		throw std::invalid_argument("Invalid grid size");
+	}
 
+	grid = new Cell*[line];
+	for (int i = 0; i < line; i++) {
+		grid[i] = new Cell[column];
+	}
+
+}
+
+Grid::~Grid() {
+	for (int i = 0; i < line; i++) {
+		delete[] grid[i];
+	}
+	delete[] grid;
+}
+
+int Grid::getLine() const {
+	return line;
+}
+
+int Grid::getColumn() const {
+	return column;
+}
+
+Cell& Grid::getCell(int line, int column) {
+	if (i < 0 || i >= line || j < 0 || j >= column) {
+		throw std::invalid_argument("Invalid cell position");
+	}
+	return grid[i][j];
 }
--- src/map/grid.h
+++ src/map/grid.h
@@ -1,14 +1,21 @@
 #pragma once
 
+#include "cell.h"
+
 class Grid {
 
 public:
 	Grid(int line, int column);
 	~Grid();
 
+	int getLine() const;
+	int getColumn() const;
+
+	Cell& getCell(int line, int column);
+
 private:	
 
 	int line;
 	int column;
-	Cell[][] grid;
+	Cell** grid;
 };
