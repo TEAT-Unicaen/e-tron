569e015f420afa247bb780ce59491f3190ae6fb8
Tomy2712 <tomrousee@gmail.com>
2025-01-29 11:52:39 +0100

    Change Cmake with new Flags | Add new executable for windows | The first windows

---
+++ src/winMain.cpp
@@ -0,0 +1,35 @@
+#include <windows.h>
+
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	
+	const auto pClassName = "E-tronClass";
+	// register window class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+
+	// create window instance
+	HWND hWnd = CreateWindowEx(
+		0, pClassName,
+		"E-tron",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr, nullptr, hInstance, nullptr
+	);
+
+	ShowWindow(hWnd, SW_SHOW);
+	while (true);
+	return 0;
+}
