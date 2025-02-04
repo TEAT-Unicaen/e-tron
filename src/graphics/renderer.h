8b20cdc567f2b706b49e00812d90fd070ce799c7
ImThe404 <jonathan.aussant@orange.fr>
2025-02-04 16:26:40 +0100

    Ajout struct Vertex + fin de sceance

--- src/graphics/renderer.h
+++ src/graphics/renderer.h
@@ -5,6 +5,12 @@
 
 #include "window.h"
 
+struct Vertex {
+	float x, y, z;  // Position dans l'espace 3D
+	float r, g, b, a;  // Couleur RGBA
+};
+
+
 class Renderer {
 
 public:
@@ -22,4 +28,9 @@ private:
 	ID3D11DeviceContext* deviceContext;
 	IDXGISwapChain* swapChain;
 	ID3D11RenderTargetView* renderTargetView;
+
+
+	ID3D11Buffer* vertexBuffer;  // Buffer pour les vertices de la grille
+	UINT vertexCount;  // Nombre de vertices
+
 };
