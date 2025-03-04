7947f0b69f1f3a80a5f87b11b8629986a3842dd0
hysteryx <theo.sicot.dev@gmail.com>
2025-03-04 14:46:07 +0100

    back button

--- web/src/components/layout/SideBar.tsx
+++ web/src/components/layout/SideBar.tsx
@@ -1,12 +1,13 @@
 import styles from "./SideBar.module.scss";
 
-
 const SideBar: React.FC = ({}) => {
     
     return (
         <>
             <div className={styles.sideBar}>
-
+                <img src="/back.svg" onClick={() => {
+                    window.location.href = "/";
+                }}></img>
             </div>
         </>
     )
